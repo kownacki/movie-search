266d4fe7633b9fa5d97053f8732ab622ed751635
Michał Kownacki <mk@michalkownacki.pl>
2020-07-14 00:16:34 +0200

    Refactor OMDb.ts. Search only for movies.

--- src/OMDb.ts
+++ src/OMDb.ts
@@ -5,10 +5,12 @@ interface SearchResult {
   isSuccess: boolean,
 }
 
+const OMDB_API_URL = 'http://www.omdbapi.com/';
+
 export const search = async (query: string): Promise<SearchResult> => {
   // todo add more than 10
   // todo api key
-  const response = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`);
+  const response = await fetch(`${OMDB_API_URL}?s=${query}&type=movie&apikey=${process.env.REACT_APP_OMDB_API_KEY}`);
   return response.status === 200
     ? {movies: (await response.json()).Search, isSuccess: true}
     : {isSuccess: false}
