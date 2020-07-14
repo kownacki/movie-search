375b82c7582dce3340b079b478661c2b3d04b6b5
Michał Kownacki <mk@michalkownacki.pl>
2020-07-14 22:05:05 +0200

    Add h1.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,6 +10,7 @@ interface AppProps {
 export const App = ({className}: AppProps): React.ReactElement => {
   return (
     <div className={className}>
+      <h1>Movie Search</h1>
       <Search />
     </div>
   );
@@ -18,5 +19,10 @@ export const App = ({className}: AppProps): React.ReactElement => {
 export default styled(App)`
   margin: auto;
   max-width: 700px;
-`;
 
+  h1 {
+    font-weight: normal;
+    font-size: 30px;
+    text-align: center;
+  }
+`;
