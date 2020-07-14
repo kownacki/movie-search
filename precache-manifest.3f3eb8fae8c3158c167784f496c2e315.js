83c10efcd4844205e1fb647e6d016269bf809418
Michał Kownacki <mk@michalkownacki.pl>
2020-07-14 05:17:26 +0200

    Updates

---
+++ precache-manifest.3f3eb8fae8c3158c167784f496c2e315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c395efd91034ea1ecd89cab4d47dee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d788ea23e20b0e7a206",
+    "url": "/static/css/main.1c6102e8.chunk.css"
+  },
+  {
+    "revision": "21541d48ac2baadcdd9b",
+    "url": "/static/js/2.cf1e96d8.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.cf1e96d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d788ea23e20b0e7a206",
+    "url": "/static/js/main.f1ed9f90.chunk.js"
+  },
+  {
+    "revision": "7dcfc3fe069edf084e5c",
+    "url": "/static/js/runtime-main.c85cac17.js"
+  }
+]);
